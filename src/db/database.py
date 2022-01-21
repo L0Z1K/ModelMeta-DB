8a1b0ab3c219933c0b73825e4016fe136d724b52
L0Z1K <qortmddbs731@naver.com>
2022-01-21 11:02:28 +0900

    feat: Database dependency

--- src/db/database.py
+++ src/db/database.py
@@ -1,3 +1,25 @@
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from src.configurations import DBConfigurations
 
 Base = declarative_base()
+
+engine = create_engine(
+    DBConfigurations.sql_alchemy_db_url,
+    encoding="utf-8",
+    pool_recycle=3600,
+    echo=False,
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    except:
+        db.rollback()
+        raise
+    finally:
+        db.close()
