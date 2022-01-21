c7077592c38e3e150f19c01607c9abccdb0fb16e
L0Z1K <qortmddbs731@naver.com>
2022-01-21 15:00:26 +0900

    build: Docker

--- src/configurations.py
+++ src/configurations.py
@@ -2,7 +2,7 @@ import os
 
 
 class DBConfigurations:
-    postgres_username = os.getenv("POSTGRES_USERNAME")
+    postgres_username = os.getenv("POSTGRES_USER")
     postgres_password = os.getenv("POSTGRES_PASSWORD")
     postgres_port = int(os.getenv("POSTGRES_PORT", 5432))
     postgres_db = os.getenv("POSTGRES_DB")
